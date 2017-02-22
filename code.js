7cc986ac87274d9a0e2f61d2c3fac0c3311fe486
Emil Kirkegaard <emil@emilkirkegaard.dk>
2017-02-22 21:40:57 +0100

    Use the first match again.
    This is a better heuristic.

--- code.js
+++ code.js
@@ -1,31 +1,30 @@
-javascript:(function(){
-  var body = document.documentElement.innerHTML;
+javascript:(function() {
+    var body = document.documentElement.innerHTML; 
+    /*    
+    * Actually it's really complicated to match DOI because of poor standards!   
+    * We adopt a broad better-to-get-false-positive-than-false-negative approach:   
+    * (word boundary) 10 . anything not (whitespace, %, ", #, ?)   
+    * http://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page   
+    * https://www.doi.org/doi_handbook/2_Numbering.html    
+    */
 
-  /*
-   * Actually it's really complicated to match DOI because of poor standards!
-   * We adopt a broad better-to-get-false-positive-than-false-negative approach:
-   * (word boundary) 10 . anything not (whitespace, %, ", #, ?)
-   * http://stackoverflow.com/questions/27910/finding-a-doi-in-a-document-or-page
-   * https://www.doi.org/doi_handbook/2_Numbering.html
-   */
-  var re = /\b10\.[^\s\%"#\?,']+\/[^\s\%"#\?,']+/g;
+    var re = /\b10\.[^\s\%"#\?,']+\/[^\s\%"#\?,']+/g; 
+    /*    
+    * TODO Action based on length    
+    * >0: Go to scihub with that DOI    
+    * >1: Let user know we found more than one, choose which to use    
+    *  0: Let user know we didnt find anything    
+    */
 
-  /*
-   * TODO Action based on length
-   *
-   * >0: Go to scihub with that DOI
-   * >1: Let user know we found more than one, choose which to use
-   *  0: Let user know we didnt find anything
-   */
-  var matches = body.match(re);
-  if (matches === null) {
-    alert("Could not find any DOI!");
-    return undefined;
-  }
+    var matches = body.match(re);
+    if (matches === null) {
+        alert("Could not find any DOI!");
+        return undefined;
+    }
 
-  /* Use the shortest match for now */
-  var doi = matches.reduce(function (a, b) { return a.length < b.length ? a : b; });
+    /* use first match */
+    doi = matches[0];
 
-  /* Go go go! */
-  window.location = "http://sci-hub.cc/" + doi;
+    /* Go go go! */
+    window.location = "http://sci-hub.cc/" + doi;
 })();
